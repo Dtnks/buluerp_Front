2eefb83b8666dc1b46e3846b09f77400f0bf0083
Dtnks <3070228868@qq.com>
2025-04-22 14:53:49 +0800

    first change

---
+++ buluerp_front/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import LayoutLeft from '@/views/layout/LayoutLeft.vue'
+import LayoutTop from '@/views/layout/LayoutTop.vue'
+</script>
+
+<template>
+  <div class="row">
+    <LayoutLeft />
+    <div class="col">
+      <LayoutTop></LayoutTop>
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ buluerp_front/src/views/layout/LayoutLeft.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { Document, Menu as IconMenu, Location, Setting } from '@element-plus/icons-vue'
+</script>
+<template>
+  <el-menu
+    id="layout"
+    active-text-color="#fff"
+    background-color="rgba(0, 21, 41, 1)"
+    class="el-menu-vertical-demo"
+    default-active="2"
+    text-color="#fff"
+  >
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>Navigator One</span>
+      </template>
+      <el-menu-item index="1-1">item one</el-menu-item>
+      <el-menu-item index="1-2">item two</el-menu-item>
+      <el-menu-item index="1-3">item three</el-menu-item>
+    </el-sub-menu>
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <span>Navigator Two</span>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <el-icon><document /></el-icon>
+      <span>Navigator Three</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon><setting /></el-icon>
+      <span>Navigator Four</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+<style scoped>
+#layout {
+  width: 20vw;
+  height: 100vh;
+}
+.el-menu-item:hover {
+  outline: 0 !important;
+  background: #1890ff !important;
+}
+.el-menu-item.is-active {
+  color: #fff !important;
+  background: #1890ff !important;
+}
+</style>
