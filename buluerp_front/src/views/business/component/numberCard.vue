62d31ca2062396bbe72f6766da0477df60f8c75b
Dtnks <3070228868@qq.com>
2025-04-24 20:06:32 +0800

    card

--- buluerp_front/src/views/business/component/numberCard.vue
+++ buluerp_front/src/views/business/component/numberCard.vue
@@ -3,7 +3,6 @@ const props = defineProps({
   textTop: { type: String },
   number: { type: String },
   dataTop: { type: Array },
-  textBottom: { type: String },
   dataBottom: { type: Array },
 })
 </script>
@@ -13,21 +12,69 @@ const props = defineProps({
       <el-text style="flex: 1">{{ textTop }}</el-text>
       <img src="@/assets/icon/u52.png" class="icon" />
     </div>
-
     <div class="Number">{{ number }}</div>
+    <div
+      v-if="dataTop.length == 2"
+      class="row"
+      style="border-bottom: 1px solid gray; padding-bottom: 3vh"
+    >
+      <el-text style="flex: 1"
+        >周同比:<img class="smallRow" v-if="dataTop[0] > 0" src="@/assets/icon/u58.png" /><img
+          v-else
+          class="smallRow"
+          src="@/assets/icon/u62.png"
+        />{{ dataTop[0] }}</el-text
+      >
+      <el-text style="flex: 1"
+        >日环比:<img class="smallRow" v-if="dataTop[1] > 0" src="@/assets/icon/u58.png" /><img
+          v-else
+          class="smallRow"
+          src="@/assets/icon/u62.png"
+        />{{ dataTop[1] }}</el-text
+      >
+    </div>
+    <div v-else-if="dataTop?.length == 1"></div>
+    <div v-if="dataBottom" class="row" style="margin-top: 8px">
+      <div v-if="dataBottom.length == 2">
+        <el-text style="flex: 1"
+          >周同比:<img class="smallRow" v-if="dataBottom[0] > 0" src="@/assets/icon/u58.png" /><img
+            v-else
+            class="smallRow"
+            src="@/assets/icon/u62.png"
+          />{{ dataBottom[0] }}</el-text
+        >
+        <el-text style="flex: 1"
+          >日环比:<img class="smallRow" v-if="dataBottom[1] > 0" src="@/assets/icon/u58.png" /><img
+            v-else
+            class="smallRow"
+            src="@/assets/icon/u62.png"
+          />{{ dataBottom[1] }}</el-text
+        >
+      </div>
+      <el-text v-else>月均营收额:{{ dataBottom[0].toLocaleString() }}</el-text>
+    </div>
   </div>
 </template>
 <style scoped>
 .showNumber {
-  background-color: aqua;
+  background-color: white;
   flex: 1;
   margin: 30px;
-  height: 20vh;
+  height: 18vh;
   padding: 25px;
+}
+.el-text {
+  margin-right: 10px;
 }
 .Number {
   font-size: 28px;
   font-weight: bold;
+  margin-bottom: 3vh;
+}
+.smallRow {
+  height: 14px;
+  width: 14px;
+  margin: 0 6px;
 }
 .icon {
   width: 16px;
--- buluerp_front/src/views/business/main/Show.vue
+++ buluerp_front/src/views/business/main/Show.vue
@@ -10,10 +10,28 @@ const number4 = 0.85
 <template>
   <div class="col" style="flex: 1; background-color: rgba(240, 242, 245, 1)">
     <div class="row">
-      <numberCard text-top="营收情况" :number="number1.toLocaleString()"></numberCard>
-      <numberCard text-top="已交付订单笔数" :number="number2.toLocaleString()"></numberCard>
-      <numberCard text-top="未交付订单" :number="number3.toLocaleString()"></numberCard>
-      <numberCard text-top="订单准时交付率" :number="number4 * 100 + '%'"></numberCard>
+      <numberCard
+        text-top="营收情况"
+        :number="number1.toLocaleString()"
+        :data-top="[12, -21]"
+        :data-bottom="[12423]"
+      ></numberCard>
+      <numberCard
+        :data-top="[12, -21, 32]"
+        text-top="已交付订单笔数"
+        :number="number2.toLocaleString()"
+      ></numberCard>
+      <numberCard
+        :data-top="[12, -21, 23]"
+        text-top="未交付订单"
+        :number="number3.toLocaleString()"
+      ></numberCard>
+      <numberCard
+        :data-top="[85]"
+        text-top="订单准时交付率"
+        :number="number4 * 100 + '%'"
+        :data-bottom="[12, 31]"
+      ></numberCard>
     </div>
     <div id="boxShow"></div>
     <div id="chart"></div>
