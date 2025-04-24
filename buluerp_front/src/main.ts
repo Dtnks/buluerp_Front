d2ab34a8a11ac451f72822b82a020f9c99f6fbec
Syfyivan <2578055724@qq.com>
2025-04-24 20:14:34 +0800

    业务订单1/2

--- buluerp_front/src/main.ts
+++ buluerp_front/src/main.ts
@@ -3,7 +3,9 @@ import { createPinia } from 'pinia'
 import '@/assets/style/main.css'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 
-app.use(createPinia()).use(router).mount('#app')
+app.use(createPinia()).use(router).use(Antd).mount('#app')
